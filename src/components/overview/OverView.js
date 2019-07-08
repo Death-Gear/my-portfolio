8418caeac75befb2bbfc89bba65840c3f5d1c86a
Yeasir Afsar <yeasirafsar.dsi@gmail.com>
2019-07-08 17:35:49 +0600

    text animation added

--- src/components/overview/OverView.js
+++ src/components/overview/OverView.js
@@ -1,35 +1,62 @@
+import $ from "jquery";
+import "textillate";
 import React from 'react';
 import './overview.css';
-import $ from "jquery";
 import profile_pic from './avatar.jpg'
 
 class OverView extends React.Component {
+
+    componentDidMount() {
+        $('#hello').textillate({
+            loop: true,
+            in: {
+                effect: 'fadeInLeft',
+                sync: 'true'
+            },
+
+            out: {
+                effect: 'fadeOutRight',
+                sync: 'true',
+            }
+        });
+
+        $('#intro').textillate({
+            loop: true,
+            in: {
+                effect: 'fadeInLeft',
+                sync: 'true'
+            },
+
+            out: {
+                effect: 'fadeOutRight',
+                sync: 'true'
+            }
+        });
+
+        $('#designation').textillate({
+            loop: true,
+            in: {
+                effect: 'fadeInLeft',
+                sync: 'true'
+            },
+
+            out: {
+                effect: 'fadeOutRight',
+                sync: 'true'
+            }
+        });
+    }
+
     render() {
         return (
             <div className="container-fluid" id="overviewWrapper">
                 <div className="row bg-primary">
                     <div className="col-md-4 col-sm-12 shadow" id="profile_content">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
-                            </div>
-                        </div>
 
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
-                            </div>
-                        </div>
+                        <h2 className="text-white" id="hello">Hello</h2>
+                        <h2 className="text-white" id="intro">I am Yeasir Afsar</h2>
+                        <h2 className="text-white" id="designation">Software Engineer</h2>
+
                     </div>
                     <img src={profile_pic} className="col-md-8 col-sm-12 img-fluid shadow " id="profile_pic"/>
                 </div>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './custom.scss';
-import '../node_modules/jquery/dist/jquery.min.js'
-import '../node_modules/popper.js/dist/popper.js'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
